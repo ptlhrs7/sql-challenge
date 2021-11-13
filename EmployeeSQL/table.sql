9b260a67545184cbc47f64e684b64c4ba3617bb2
ptlhrs7 <90519283+ptlhrs7@users.noreply.github.com>
2021-11-13 15:52:08 -0500

    Final Code Commit

---
+++ EmployeeSQL/table.sql
@@ -0,0 +1,63 @@
+-- Creating & Importing data tables 
+-- Titles
+CREATE TABLE titles(
+	title_id VARCHAR PRIMARY KEY,
+	title VARCHAR
+);
+SELECT * FROM titles;
+
+-- Employees
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR,
+	birth_date DATE,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	sex VARCHAR,
+	hire_date DATE,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+SELECT * FROM employees;
+
+-- Departments
+CREATE TABLE departments(
+	dept_no VARCHAR PRIMARY KEY,
+	dept_name VARCHAR
+); 
+SELECT * FROM departments;
+
+-- Dept_Manager
+CREATE TABLE dept_manager(
+	dept_no VARCHAR,
+	emp_no INT,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no, emp_no)
+);
+SELECT * FROM dept_manager;
+
+-- Dept_Emp
+CREATE TABLE dept_emp(
+	emp_no INT,
+	dept_no VARCHAR,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+SELECT * FROM dept_emp;
+
+-- Salaries
+CREATE TABLE salaries(
+	emp_no INT PRIMARY KEY,
+	salary INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+SELECT * FROM salaries;
+
+
+
+
+
+
+
+
